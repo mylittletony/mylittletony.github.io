cb9899f89080f7645aa9024f738a113d1e08f106
ZakMooney <zak@polkaspots.com>
2017-06-07 17:59:43 +0100

    zurb nav header

--- js/app.js
+++ js/app.js
@@ -2,4 +2,14 @@
 
 $(document).foundation();
 
+$(window).scroll(function() {
+    var scroll = $(window).scrollTop();
+    if (scroll > 0) {
+        $("#sticky-header").addClass("active");
+    }
+    else {
+        $("#sticky-header").removeClass("active");
+    }
+});
+
 console.log("%cWelcome To Cucumber", "font-size: x-large")
